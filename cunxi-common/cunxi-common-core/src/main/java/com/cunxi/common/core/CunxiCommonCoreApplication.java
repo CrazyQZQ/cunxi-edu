615a27561b21dcfa60c9c2c235bdc80223d3ab5e
CrazyQZQ <13186061107@163.com>
2023-04-24 20:19:55 +0800

    首次提交

---
+++ cunxi-common/cunxi-common-core/src/main/java/com/cunxi/common/core/CunxiCommonCoreApplication.java
@@ -0,0 +1,13 @@
+package com.cunxi.common.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CunxiCommonCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CunxiCommonCoreApplication.class, args);
+    }
+
+}
---
+++ cunxi-common/cunxi-common-core/src/test/java/com/cunxi/common/core/CunxiCommonCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cunxi.common.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CunxiCommonCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
